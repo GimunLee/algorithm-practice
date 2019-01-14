22916958fcfaea31571fb936603752ff4e29110f
GimunLee <gimoon0226@naver.com>
2019-01-14 23:24:25 +0900

    [S/W 문제해결 기본] 1일차 - 최빈수 구하기

---
+++ sw_expert_academy/src/d2/Solution_1204.java
@@ -0,0 +1,35 @@
+package d2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution_1204 {
+	static final Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int tc = sc.nextInt();
+		
+		for (int i = 1; i <= tc; i++) {
+			int tc_num = sc.nextInt();
+			int frequent = 0;
+			int index = 0;
+			int[] score = new int[101];	
+			int[] arr = new int[1000];
+			
+			for (int j = 0; j < arr.length; j++) {
+				arr[j] = sc.nextInt();
+				score[arr[j]] ++;
+			}
+			
+			for (int j = score.length-1; j >= 0; j--) {
+				if(frequent < score[j]) {
+					frequent = score[j];
+					index = j;
+				}
+			}
+			System.out.printf("#%d %d \n", tc_num, index);
+		}
+		
+	}
+
+}

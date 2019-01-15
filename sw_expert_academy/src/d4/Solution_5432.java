17180308833b346b0486bf2b4df7285fbb31dcad
GWTF <gwtf1210@naver.com>
2019-01-16 00:47:45 +0900

    쇠막대기 자르기 (내 방법)

--- sw_expert_academy/src/d4/Solution_5432.java
+++ sw_expert_academy/src/d4/Solution_5432.java
@@ -2,46 +2,38 @@ package d4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 /** 
  * 쇠막대기 자르기
  */
 public class Solution_5432 {
-    static int[] stack = new int[100];
-    static int top = -1;
+
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine()); // Test Case 수
         
-        int tc = Integer.parseInt(br.readLine());
-        
-        for (int p = 1; p <= tc; p++) {
-            int sum = 0;
-            String tmp = br.readLine();
-            String[] input = tmp.split("");
+        for (int tn = 1; tn <= tc; tn++) {
+            int sum = 0; // 잘린 막대기 개수의 합
+            int open = 0; // 열린 괄호 개수의 합
+            
+            char[] input = br.readLine().toCharArray(); // char로 저장
             
             for (int i = 0; i < input.length; i++) {
-                if(input[i].equals("(")) {
-                	top++;
-                }
-                else if(input[i].equals(")")) {
-                    top--;
-                    if(top != -1 && input[i-1].equals("(")) {
-                        sum += top + 1;
-                    }
-                    else if(top == -1){
-                        if(input[i-1].equals("(")) {
-                        }
-                        else {
-                            sum++;
-                        }
-                    }
-                    else {
-                        sum++;
-                    }
-                }
+            	char tmp = input[i];
+            
+            	if(tmp=='(') {
+            		open++;
+            	}
+            	// 닫힌 것이 나왔을 때,
+            	else if(input[i-1] == '('){ // 1. 레이저일 경우
+            		open--;
+            		sum += (open--);
+            	}else { // 2. 레이저가 아닐 경우
+            		open--;
+            		sum += 1;
+            	}
             }
-            System.out.println("#"+p+" "+sum);
+            System.out.println("#" + tn + " " + sum);
         }
     }
 }

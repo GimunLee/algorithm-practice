ae632147e859cd935bece7c4c4974675f907fce1
GWTF <gwtf1210@naver.com>
2019-01-28 18:52:47 +0900

    BOJ #11727 2xn 타일링2

---
+++ baekjoon_online_judge/src/ac_dynamic_programming1/Main_11727.java
@@ -0,0 +1,21 @@
+package ac_dynamic_programming1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_11727 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int[] d = new int[1001];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine().trim());
+		d[0] = 1;
+		d[1] = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			d[i] += (d[i-1] + 2*d[i-2])%10007;
+		}
+		
+		System.out.println(d[n]);
+	}
+}

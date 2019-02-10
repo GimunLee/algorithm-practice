85579d96f59c80b4060b5f957ce2e9d02103fdc3
GWTF <gwtf1210@naver.com>
2019-02-10 22:23:17 +0900

    백준 BFS, DFS 풀 문제

---
+++ baekjoon_online_judge/src/af_graph1/Main_1260.java
@@ -0,0 +1,32 @@
+package af_graph1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1260 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int N = Integer.parseInt(st.nextToken()); // 정점 수
+		int M = Integer.parseInt(st.nextToken()); // 간선 수
+		int V = Integer.parseInt(st.nextToken()); // 시작점
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}

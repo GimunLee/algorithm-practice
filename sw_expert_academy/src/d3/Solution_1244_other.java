34042eb217db31612f5168c56fd1b22bafea7271
GimunLee <gimoon0226@naver.com>
2019-02-12 00:30:18 +0900

    [S/W 문제해결 응용] 2일차 - 최대 상금 / Check

--- sw_expert_academy/src/d3/Solution_1244_other.java
+++ sw_expert_academy/src/d3/Solution_1244_other.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * [S/W 문제해결 응용] 2일차 - 최대 상금
+ */
 public class Solution_1244_other {
 	static int maxPrice = 0;
 	static int limit;
@@ -32,7 +34,7 @@ public class Solution_1244_other {
 
 	static void findMax(int n) {
 		int calc = calcPrice();
-		
+
 		if (n == limit) {
 			maxPrice = Math.max(maxPrice, calc);
 			return;
@@ -40,7 +42,7 @@ public class Solution_1244_other {
 
 		if (check[calc][n] == 1)
 			return;
-		
+
 		check[calc][n] = 1;
 
 		for (int i = 0; i < price.length; i++) {

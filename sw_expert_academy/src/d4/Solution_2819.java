ff454c67a732a11e393a64d0f565cff12f008a7e
GimunLee <gimoon0226@naver.com>
2019-02-17 21:29:47 +0900

    격자판의 숫자 이어 붙이기

--- sw_expert_academy/src/d4/Solution_2819.java
+++ sw_expert_academy/src/d4/Solution_2819.java
@@ -3,14 +3,12 @@ package d4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Solution_2819 {
-	static int ans;
 	static char[][] input;
-	static ArrayList<String> memo;
-	static String temp = "";
+	static HashSet<String> answer;
 	static int[][] direction = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } }; // 상 좌 우 하
 
 	static boolean inRange(int mx, int my) { // move한 곳이 범위를 벗어나는지 체크
@@ -20,36 +18,22 @@ public class Solution_2819 {
 		return true;
 	}
 
-	private static void search(int x, int y, int count) {
+	private static void search(int x, int y, String temp) {
 		temp += input[x][y];
-		count++;
 
-		if (count == 7) {
-			boolean chk = false;
-			System.out.println(temp);
-			for (int i = 0; i < memo.size(); i++) {
-				if (temp.equals(memo.get(i))) {
-					chk = true;
-					break;
-				}
-			}
-			if (!chk) {
-				memo.add(temp);
-				ans++;
-			}
-			count = 0;
-			temp = "";
+		if (temp.length() == 7) {
+			answer.add(temp);
 			return;
 		} else {
 			for (int i = 0; i < direction.length; i++) {
 				int nx = x + direction[i][0];
 				int ny = y + direction[i][1];
+
 				if (inRange(nx, ny)) {
-					search(nx, ny, count);
+					search(nx, ny, temp);
 				}
 			}
 		}
-
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -59,12 +43,11 @@ public class Solution_2819 {
 		int C = Integer.parseInt(br.readLine().trim()); // Test Case 수
 
 		for (int tc = 1; tc <= C; tc++) {
-			ans = 0;
-			memo = new ArrayList<String>();
+			answer = new HashSet<String>();
 			input = new char[4][4];
 
 			// -- input -- //
-			for (int x = 0; x < 3; x++) {
+			for (int x = 0; x < 4; x++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 				int y = 0;
 				while (st.hasMoreTokens()) {
@@ -75,11 +58,11 @@ public class Solution_2819 {
 			// -- function -- //
 			for (int x = 0; x < 4; x++) {
 				for (int y = 0; y < 4; y++) {
-					search(x, y, 0);
+					search(x, y, "");
 				}
 			}
 
-			System.out.println("#" + tc + " " + ans);
+			System.out.println("#" + tc + " " + answer.size());
 		}
 	}
 }

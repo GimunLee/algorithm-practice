8069a4a3d9fe4289eb61034a036e5aa87f9f42bc
GimunLee <gimoon0226@naver.com>
2019-02-21 21:54:42 +0900

    BOJ #10026 적록색약 (DFS, BFS)

---
+++ baekjoon_online_judge/src/af_graph1/Main_10026_적록색약_DFS.java
@@ -0,0 +1,85 @@
+package af_graph1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_10026_Àû·Ï»ö¾à_DFS {
+	static char[][] map;
+	static boolean[][] visited;
+	static int ans_normal;
+	static int ans_special;
+	static int[][] direction= {{-1,0},{1,0},{0,-1},{0,1}}; // »ó ÇÏ ÁÂ ¿ì
+	
+	private static boolean inRange(int nr, int nc) {
+		if(nr < 0 || nc <0 || nr >= map.length || nc >= map.length) {
+			return false;
+		}
+		return true;
+	}
+	
+	private static void dfs(int r, int c) {
+		visited[r][c] = true;
+		
+		for (int i = 0; i < direction.length; i++) {
+			int nr = r + direction[i][0];
+			int nc = c + direction[i][1];
+			
+			if(!inRange(nr,nc) || visited[nr][nc] || map[r][c] != map[nr][nc]) {
+				continue;
+			}
+			
+			visited[nr][nc] = true;
+			dfs(nr,nc);
+		}
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine().trim()); // N x N Çà·Ä
+
+		map = new char[N][N];
+		visited = new boolean[N][N];
+
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = str.charAt(j);
+			}
+		}
+
+		// 1. ÀÏ¹ÝÀÎ Å½»ö
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				if(!visited[i][j]) {
+					dfs(i,j);
+					ans_normal++;
+				}
+			}
+		}
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				visited[i][j] = false;
+				if(map[i][j] == 'R') {
+					map[i][j] = 'G';
+				}
+			}
+		}
+		
+		// 2. »ö¸Í Å½»ö
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				if(!visited[i][j]) {
+					dfs(i,j);
+					ans_special++;
+				}
+			}
+		}
+
+		System.out.println(ans_normal + " " + ans_special);
+
+	} // end of main
+} // end of class

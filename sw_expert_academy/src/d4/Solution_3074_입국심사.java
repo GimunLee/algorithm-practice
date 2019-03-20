d0c2f8bdf9d390e127f2165e947dbdf1f98bc96c
GimunLee <gimoon0226@naver.com>
2019-03-20 15:29:41 +0900

    SWEA Not Yet

--- sw_expert_academy/src/d4/Solution_3074_입국심사.java
+++ sw_expert_academy/src/d4/Solution_3074_입국심사.java
@@ -5,22 +5,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution_3074_입국심사 {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
+		int TC = Integer.parseInt(br.readLine());
+		for (int testCase = 1; testCase <= TC; testCase++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int N = Integer.parseInt(st.nextToken()); // 심사대개수 1<=N<=10^5
-			int M = Integer.parseInt(st.nextToken()); // 통과할인원수 1<=M<=10^9
+			int N = Integer.parseInt(st.nextToken()); // 1 <= N <= 10^5, 심사대 개수
+			int M = Integer.parseInt(st.nextToken()); // 1 <= M <= 10^9, 통과할 인원수
 			int[] t = new int[N];
-			for (int i = 0; i < t.length; i++) {
-				t[i] = Integer.parseInt(br.readLine().trim());
+			for (int i = 0; i < N; i++) {
+				t[i] = Integer.parseInt(br.readLine().trim()); // 1 <= tk <= 10^9
 			}
 			long start = 0;
 			long end = 1000000000L * M;
 			long min = Long.MAX_VALUE; // time 최소값
-			while (start <= end) {
+			while(start <= end) {
 				long mid = (start + end) / 2;
 				long cnt = 0; // 정해진 mid 시간안에 통과할 수 있는 인원수
 				for (int i = 0; i < t.length; i++) {
@@ -29,13 +28,17 @@ public class Solution_3074_입국심사 {
 				if (cnt >= M && min > mid) {
 					min = mid;
 				}
+			
 				if (cnt < M) {
 					start = mid + 1;
 				} else if (M <= cnt) {
 					end = mid - 1;
 				}
-			}
-			System.out.println(min);
-		}
-	}// end of main
-}// end of class
+			} // end of while
+			System.out.println("#"+testCase+" "+min);
+		} // end of for testCase
+	}
+}
+
+
+

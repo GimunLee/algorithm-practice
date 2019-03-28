42c7e3961ca9bd23b757334f958279974989fd14
GimunLee <gimoon0226@naver.com>
2019-03-28 22:13:32 +0900

    Solution of [DP]

---
+++ baekjoon_online_judge/src/new_mind/Main_2294_동전2.java
@@ -0,0 +1,55 @@
+package new_mind;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2294_동전2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken()); // 동전의 개수
+		int k = Integer.parseInt(st.nextToken()); // 동전의 가치
+		int[] money = new int[n];
+
+		int[] dp = new int[k + 1];
+
+		for (int i = 0; i < n; i++) {
+			money[i] = Integer.parseInt(br.readLine().trim());
+		}
+
+		Arrays.sort(money);
+
+		for (int i = 0; i < n; i++) {
+			int temp = 1;
+			for (int j = money[i]; j < dp.length; j += money[i]) {
+				dp[j] = temp++;
+			}
+		}
+		
+		for (int i = 0; i < dp.length; i++) {
+			if(dp[i] == 0) {
+				dp[i] = Integer.MAX_VALUE;
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = money[i]+1; j < dp.length; j++) {
+				if(dp[j-money[i]] != Integer.MAX_VALUE) { // 되는 경우
+					dp[j] = Math.min(dp[j-money[i]]+1,dp[j]);
+				}
+			}
+		}
+		
+//		System.out.println(Arrays.toString(dp));
+
+		if (dp[k] == Integer.MAX_VALUE) {
+			dp[k] = -1;
+		}
+		System.out.println(dp[k]);
+	}
+}
---
+++ sw_expert_academy/src/d3/Solution_5215_햄버거다이어트_DP.java
@@ -0,0 +1,46 @@
+package d3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_5215_햄버거다이어트_DP {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int TC = Integer.parseInt(br.readLine().trim()); // Test Case 수
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int tc = 1; tc <= TC; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " "); // N, L
+
+			int N = Integer.parseInt(st.nextToken()); // 햄버거 재료 수
+			int L = Integer.parseInt(st.nextToken()); // 제한 칼로리
+
+			int[][] set = new int[N][2];
+
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+
+				int score = Integer.parseInt(st.nextToken()); // 점수
+				int calory = Integer.parseInt(st.nextToken()); // 칼로리
+
+				set[i][0] = score; // 점수
+				set[i][1] = calory; // 칼로리
+			}
+
+			int[] dp = new int[L + 1];
+
+			for (int i = 0; i < set.length; i++) {
+				for (int j = L; j > set[i][1]; j--) {
+					dp[j] = Math.max(dp[j], dp[j - set[i][1]] + set[i][0]);
+				}
+			}
+			sb.append('#').append(tc).append(' ').append(dp[L]).append('\n');
+		}
+		System.out.println(sb.toString());
+	}
+}

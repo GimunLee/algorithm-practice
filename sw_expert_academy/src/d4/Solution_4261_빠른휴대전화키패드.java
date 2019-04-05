99966205dde63d7e182137f928e6081a0d4dd3cd
GWTF <gwtf1210@naver.com>
2019-04-05 09:53:45 +0900

    SWEA #4261 빠른휴대전화키패드

--- sw_expert_academy/src/d4/Solution_4261_빠른휴대전화키패드.java
+++ sw_expert_academy/src/d4/Solution_4261_빠른휴대전화키패드.java
@@ -24,31 +24,37 @@ public class Solution_4261_빠른휴대전화키패드 {
 			for (int i = 0; i < N; i++) {
 				arr[i] = st.nextToken();
 				String temp = "";
-				
+
 				for (int j = 0; j < arr[i].length(); j++) {
 					temp += alphaToString(arr[i].charAt(j));
 				}
-				
-				if(temp.equals(S)) {
+
+				if (temp.equals(S)) {
 					ans++;
 				}
 			}
-			
-						
-			
 			System.out.println("#" + tc + " " + ans);
 		} // end of for TestCase
-		
+
 	} // end of main
+
 	private static String alphaToString(char c) {
-		if(c == 'a' || c == 'b' || c == 'c') return "2";
-		if(c == 'd' || c == 'e' || c == 'f') return "3";
-		if(c == 'g' || c == 'h' || c == 'i') return "4";
-		if(c == 'j' || c == 'k' || c == 'l') return "5";
-		if(c == 'm' || c == 'n' || c == 'o') return "6";
-		if(c == 'p' || c == 'q' || c == 'r' || c== 's') return "7";
-		if(c == 't' || c == 'u' || c == 'v') return "8";
-		if(c == 'w' || c == 'x' || c == 'y' || c== 'z') return "9";
+		if (c == 'a' || c == 'b' || c == 'c')
+			return "2";
+		if (c == 'd' || c == 'e' || c == 'f')
+			return "3";
+		if (c == 'g' || c == 'h' || c == 'i')
+			return "4";
+		if (c == 'j' || c == 'k' || c == 'l')
+			return "5";
+		if (c == 'm' || c == 'n' || c == 'o')
+			return "6";
+		if (c == 'p' || c == 'q' || c == 'r' || c == 's')
+			return "7";
+		if (c == 't' || c == 'u' || c == 'v')
+			return "8";
+		if (c == 'w' || c == 'x' || c == 'y' || c == 'z')
+			return "9";
 		return "0";
 	}
 } // end of class

006327eb031cfc327e89d9cd8e96206ae0693e2b
GWTF <gwtf1210@naver.com>
2019-06-27 17:31:02 +0900

    Upload

--- sw_expert_academy/src/d4/Solution_5672_올해의조련사.java
+++ sw_expert_academy/src/d4/Solution_5672_올해의조련사.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+/*
+ * Upload 
+ * */
+
 public class Solution_5672_올해의조련사 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

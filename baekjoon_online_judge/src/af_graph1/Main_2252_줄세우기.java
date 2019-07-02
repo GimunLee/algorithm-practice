717c08101bf8d8f7cabb37fd91feeea231d9ca88
GimunLee <gimoon0226@naver.com>
2019-07-03 01:25:49 +0900

    BOJ #2252 줄세우기

--- baekjoon_online_judge/src/af_graph1/Main_2252_줄세우기.java
+++ baekjoon_online_judge/src/af_graph1/Main_2252_줄세우기.java
@@ -3,6 +3,7 @@ package af_graph1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -32,13 +33,21 @@ public class Main_2252_줄세우기 {
 			list[A].add(B);
 		}
 
+		ArrayList<Integer> tlist = new ArrayList<Integer>();
+
 		for (int i = 1; i <= N; i++) {
-			if (list[i] != null) { // 나가는 간선있을 때
+			if (list[i] != null && !visited[i]) { // 나가는 간선있을 때
 				dfs(i);
 			}
 		}
-
 		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				sb.append(i).append(" ");
+			}
+		}
+
 		while (!stack.isEmpty()) {
 			sb.append(stack.pop()).append(" ");
 		}
@@ -47,19 +56,19 @@ public class Main_2252_줄세우기 {
 
 	private static void dfs(int student) {
 		visited[student] = true;
-		
-		if(list[student] == null) {
+
+		if (list[student] == null) {
 			stack.add(student);
 			return;
 		}
-		
+
 		for (int i = 0; i < list[student].size(); i++) {
 			int next = list[student].get(i);
-			if(visited[next]) {
+			if (visited[next]) {
 				continue;
 			}
 			dfs(next);
-			
+
 		}
 		stack.add(student);
 	} // end of func(dfs)

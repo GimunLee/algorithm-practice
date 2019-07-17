1ea2bc64e6fab967848674f24d5bb8e116321bf4
GimunLee <gimoon0226@naver.com>
2019-07-18 01:37:49 +0900

    NY

---
+++ baekjoon_online_judge/src/samsung/Main_2631_줄세우기.java
@@ -0,0 +1,20 @@
+package samsung;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2631_줄세우기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine().trim());
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(br.readLine().trim());
+			
+			
+		}
+
+	}
+}

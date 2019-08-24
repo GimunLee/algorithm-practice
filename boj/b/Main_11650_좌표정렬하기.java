992c514cc9aba35ac570f32abbfbe034434f0e56
Ricky <gwtf1210@naver.com>
2019-08-24 23:56:42 +0900

    Commit Test / Check the another setting

--- boj/b/Main_11650_좌표정렬하기.java
+++ boj/b/Main_11650_좌표정렬하기.java
@@ -1,101 +1,28 @@
 package boj.b;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-/** Remind */
-public class Main_11650_��ǥ�����ϱ� {
-	private static int N;
-	private static Pair[] input;
+class Main_11650_��ǥ�����ϱ� {
+	private static Pair input[];
+	private static int num;
+	
+	public static void main(String arg[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		input = new Pair[num];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine().trim()); // 1 <= N <= 100,000
-		input = new Pair[N];
-
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			input[i] = new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		} // end of for(input)
-//		quickSort(0, N - 1);
-		sort(input,0,N-1);
-		
-		System.out.println(Arrays.toString(input));
-
-	} // end of main
-
-	private static void quickSort(int first, int last) {
-
-		if (first >= last) {
-			return;
+		for (int i = 0; i < num; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			input[i] = new Pair(x, y);
 		}
 
-		int pivot = (first + last) / 2;
-		Pair pivotValue = input[pivot];
-		int i = first;
-		int j = last;
-
-		while (i < j) { // Partition
-			System.out.println(i + ", " + j);
-			System.out.println(Arrays.toString(input));
-			while (input[i].x <= pivotValue.x) {
-				i++;
-			}
-			while (input[j].x > pivotValue.x) {
-				j--;
-			}
-
-			if (i <= j) {
-				Pair temp = input[i];
-				input[i] = input[j];
-				input[j] = temp;
-				i++;
-				j--;
-			}
-		} // end of Partition
-		pivot = i;
-		// i�� ����
-		quickSort(first, pivot - 1);
-		quickSort(pivot + 1, last);
+		quickSort(0, num - 1);
+		printResult();
+		sc.close();
 	}
 	
-	public static void sort(Pair[] data, int l, int r) {
-        int left = l;
-        int right = r;
-        Pair pivot = data[(l + r) / 2]; // pivot ��� ���� (�־��� ��� ����)
- 
-        do {
-            while (data[left].x < pivot.x)
-                left++;
-            while (data[right].x > pivot.x)
-                right--;
- 
-            if (left <= right) {
-                System.out.println("change");
-                Pair temp = data[left];
-                data[left] = data[right];
-                data[right] = temp;
-                left++;
-                right--;
-            }
-            System.out.println(Arrays.toString(data)+"  "+pivot);
-            System.out.println("left : " + left + " right : " + right);
-        } while (left <= right);
- 
-        if (l < right) {//���������� �� �̻� ���� ���� ���� ��� üũ (���� ������ �� �ִ��� üũ)
-            System.out.println("l : " + l + " end: " + right);
-            sort(data, l, right);
-        }
-        if (r > left) {//���������� �� �̻� ���� ���� ���� ��� üũ (������ ������ �� �ִ��� üũ)
-            System.out.println("l : " + left + " end: " + r);
-            sort(data, left, r);
- 
-        }
-    }
-
-	private static class Pair {
+	static class Pair {
 		int x;
 		int y;
 
@@ -103,11 +30,54 @@ public class Main_11650_��ǥ�����ϱ� {
 			this.x = x;
 			this.y = y;
 		}
+	}
 
-		@Override
-		public String toString() {
-			return "Pair [x=" + x + ", y=" + y + "]";
+	static void quickSort(int first, int last) {
+		Pair temp;
+		if (first < last) {
+			int pivot = first;
+			int i = first;
+			int j = last;
+
+			while (i < j) {
+				while (input[i].x <= input[pivot].x && i < last) {
+					if (input[i].x == input[pivot].x) {
+						if (input[i].y > input[pivot].y) {
+							break;
+						}
+					}
+					i++;
+				}
+				while (input[j].x >= input[pivot].x) {
+					if (input[j].x == input[pivot].x) {
+						if (input[j].y <= input[pivot].y) {
+							break;
+						}
+					}
+					j--;
+				}
+				if (i < j) {
+					temp = input[i];
+					input[i] = input[j];
+					input[j] = temp;
+				}
+			}
+
+			temp = input[pivot];
+			input[pivot] = input[j];
+			input[j] = temp;
+
+			quickSort(first, j - 1);
+			quickSort(j + 1, last);
 		}
+	}
 
-	} // end of Pair
-} // end of class
+	static void printResult() {
+		for (int i = 0; i < num; ++i) {
+			System.out.print(input[i].x + " " + input[i].y);
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+}

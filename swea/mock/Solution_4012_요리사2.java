6b2db17eac2ff6726f3388e3a0cf46228ba06a70
GimunLee <gimoon0226@naver.com>
2019-08-27 16:57:46 +0900

    SWEA 요리사

--- swea/mock/Solution_4012_요리사2.java
+++ swea/mock/Solution_4012_요리사2.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_4012_요리사 {
+/**
+ * 2019.08.27.(화)
+ * */
+public class Solution_4012_요리사2 {
 	private static int N;
 	private static int[][] map;
 
@@ -23,62 +26,65 @@ public class Solution_4012_요리사 {
 					map[r][c] = Integer.parseInt(st.nextToken());
 				}
 			}
-			permuSet1 = new int[N / 2];
-			permuSet2 = new int[N / 2];
-
-			combSet = new int[2];
+			solveSet1 = new int[N / 2];
+			solveSet2 = new int[N / 2];
 			visited = new boolean[N + 1];
 			ANS = Integer.MAX_VALUE;
-			permu(0);
+			combSet = new int[N / 2];
+			solve(0, 1);
 			sb.append("#").append(tc).append(" ").append(ANS).append("\n");
 		}
 		System.out.println(sb.toString());
 	} // end of main
 
-	private static int[] permuSet1;
-	private static int[] permuSet2;
+	private static int[] solveSet1;
+	private static int[] solveSet2;
 	private static boolean[] visited;
 	private static int ANS;
 
-	private static void permu(int len) {
-		if (len == N) {
+	private static void solve(int len, int idx) {
+		if (len == N / 2) {
+			int tmpIdx = 0;
+			for (int i = 1; i < visited.length; i++) {
+				if (!visited[i]) {
+					solveSet2[tmpIdx++] = i;
+				}
+			}
 			value = 0;
-			comb(permuSet1, 0, 0);
+			comb(solveSet1, 0, 0);
 			int a = value;
 			value = 0;
-			comb(permuSet2, 0, 0);
+			comb(solveSet2, 0, 0);
 			int b = value;
 			int tmp = Math.abs(a - b);
 			ANS = ANS > tmp ? tmp : ANS;
 			return;
 		}
 
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				if (len < N / 2) {
-					permuSet1[len] = i;
-				} else {
-					permuSet2[len - N / 2] = i;
-				}
-				permu(len + 1);
-				visited[i] = false;
+		for (int i = idx; i <= N; i++) {
+			solveSet1[len] = i;
+			if (solveSet1[0] != 1) {
+				return;
 			}
+			visited[i] = true;
+			solve(len + 1, i + 1);
+
+			visited[i] = false;
 		}
-	} // end of func(permu)
+	} // end of func(solve)
 
 	private static int[] combSet;
 	private static int value;
 
-	private static void comb(int[] inputpermuSet1, int idx, int len) {
+	private static void comb(int[] inputSolveSet, int idx, int len) {
 		if (len == 2) {
 			value += map[combSet[0]][combSet[1]] + map[combSet[1]][combSet[0]];
 			return;
 		}
 
 		for (int i = idx; i < N / 2; i++) {
-			combSet[len] = inputpermuSet1[i];
-			comb(inputpermuSet1, i + 1, len + 1);
+			combSet[len] = inputSolveSet[i];
+			comb(inputSolveSet, i + 1, len + 1);
 		}
 	}
 } // end of class

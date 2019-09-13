06c92f0c3850c1956dbea5ff61a163d00d76bb29
GimunLee <gimoon0226@naver.com>
2019-09-13 17:05:13 +0900

    PG 체육복

---
+++ programmers/Solution.java
@@ -0,0 +1,48 @@
+package programmers;
+
+import java.util.Arrays;
+
+class Solution {
+
+	public static void main(String[] args) {
+		int[] lost = { 1, 3, 5 };
+		int[] reservce = { 1, 4 };
+		int ans = solution(5, lost, reservce);
+		System.out.println(ans);
+
+	}
+
+	public static int solution(int n, int[] lost, int[] reserve) {
+		Arrays.sort(lost);
+		Arrays.sort(reserve);
+		int answer = n;
+		int[] cnt = new int[n + 2];
+
+		for (int i = 1, j = 0, k = 0; i <= n; i++) {
+			cnt[i] = 1;
+			if (j < lost.length && i == lost[j]) {
+				cnt[i]--;
+				j++;
+			}
+			if (k < reserve.length && i == reserve[k]) {
+				cnt[i]++;
+				k++;
+			}
+		}
+		for (int i = 0; i < lost.length; i++) {
+			int cur = lost[i];
+			if (cnt[cur] == 1) {
+				continue;
+			}else if (cnt[cur - 1] >= 2) {
+				cnt[cur - 1]--;
+				continue;
+			}else if (cnt[cur +1] >= 2) {
+				cnt[cur +1]--;
+				continue;
+			}else {
+				answer--;
+			}
+		}
+		return answer;
+	}
+}

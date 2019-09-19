31d317567b565aa4899c121f09f62be5055325d2
Ricky <gwtf1210@naver.com>
2019-09-19 19:07:47 +0900

    PG 예산

---
+++ programmers/Solution_예산.java
@@ -0,0 +1,49 @@
+package programmers;
+
+public class Solution_예산 {
+	public static void main(String[] args) {
+		int[] input = { 130, 130, 130, 130 };
+		int M = 485;
+
+		System.out.println(solution(input, M));
+
+	}
+
+	public static int solution(int[] budgets, int M) {
+		int answer = 0;
+		int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+
+		for (int i = 0; i < budgets.length; i++) {
+			min = Math.min(budgets[i], min);
+			max = Math.max(budgets[i], max);
+		} // 최소 최대 뽑기
+
+		int start = 0;
+		int end = max;
+
+		while (true) {
+			if (start > end) {
+				break;
+			}
+
+			int mid = (start + end) / 2;
+
+			long tmpSum = 0;
+			for (int i = 0; i < budgets.length; i++) {
+				if (budgets[i] > mid) {
+					tmpSum += mid;
+				} else {
+					tmpSum += budgets[i];
+				}
+			}
+
+			if (tmpSum > M) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+				answer = Math.max(answer, mid);
+			}
+		}
+		return answer;
+	} // end of func(solution)
+}

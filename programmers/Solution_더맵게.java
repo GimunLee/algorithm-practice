5591d162f2f49e6b9ac8132737aa9e215a331175
GimunLee <gimoon0226@naver.com>
2019-09-26 17:21:13 +0900

    PG 더맵게

---
+++ programmers/Solution_더맵게.java
@@ -0,0 +1,40 @@
+package programmers;
+
+import java.util.PriorityQueue;
+
+public class Solution_´õ¸Ê°Ô {
+	public static void main(String[] args) {
+		int[] scoville = { 1, 2, 3, 9, 10, 12 };
+		int K = 10000;
+
+		System.out.println(solution(scoville, K));
+
+	}
+
+	public static int solution(int[] scoville, int K) {
+		int answer = 0;
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+		for (int i = 0; i < scoville.length; i++) {
+			queue.add(scoville[i]);
+		}
+		boolean flag = false;
+		while (!queue.isEmpty()) {
+			int food1 = queue.poll();
+			if (food1 >= K) {
+				flag = true;
+				break;
+			}
+			answer++;
+			if (queue.isEmpty()) {
+				break;
+			}
+			int food2 = queue.poll() * 2;
+			int sum = food1 + food2;
+			queue.add(sum);
+		}
+		if (!flag) {
+			answer = -1;
+		}
+		return answer;
+	}
+}
